4fbd18e16ba8156ccf3b35d3d1300d510ac43349
jamesfalkner <schtool@gmail.com>
2019-04-18 11:38:07 -0400

    health

---
+++ src/main/java/org/acme/testapp/rest/HealthEndpoint.java
@@ -0,0 +1,20 @@
+package org.acme.testapp.rest;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@ApplicationScoped
+@Path("/")
+public class HealthEndpoint implements Serializable {
+
+    private static final long serialVersionUID = -2630425175177495070L;
+
+    @GET
+    public String alive() {
+        return "UP";
+    }
+
+}
